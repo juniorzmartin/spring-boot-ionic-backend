0b66ced0f5c700c8245659b8d5f364e260ec669c
Ednilson Martins <juniorzmartin@gmail.com>
2022-06-20 10:39:25 -0300

    Endpoints para buscar estados e cidades

--- src/main/java/com/edmartins/cursomc/repositories/EstadoRepository.java
+++ src/main/java/com/edmartins/cursomc/repositories/EstadoRepository.java
@@ -1,9 +1,15 @@
 package com.edmartins.cursomc.repositories;
 
+import java.util.List;
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.edmartins.cursomc.domain.Estado;
 
 public interface EstadoRepository extends JpaRepository<Estado, Integer>{
-
+	
+	@Transactional(readOnly = true)
+	public List<Estado> findAllByOrderByNome();
 }
